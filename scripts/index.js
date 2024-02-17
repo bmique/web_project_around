c1f33075660d149173affc5c9abfb0834d07222c
Betsabé Mique <betsabemique@gmail.com>
2024-02-17 12:23:51 -0300

    feat: agregar funciones popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,47 +1,22 @@
 const profileNameElement = document.querySelector(".profile__name");
 const profileAboutElement = document.querySelector(".profile__info");
 
+const nameInput = document.querySelector("#input-name");
+const JobInput = document.querySelector("#input-about");
 const profileName = profileNameElement.textContent;
 const profileAbout = profileAboutElement.textContent;
 
+const profileSaveButton = document.querySelector("#profile-save-button");
+
 const profileEditButton = document.querySelector(".profile__edit-button");
-const editPopupElement = document.querySelector(".popup");
+const editPopupElement = document.querySelector("#popup-profile");
 
 const formElement = document.querySelector(".popup__form");
-const nameInput = document.querySelector(".popup__input_type_name");
-const JobInput = document.querySelector(".popup__input_type_about");
-const closeButtonPopup = document.querySelector(".popup__button-close");
+const profileCloseButton = document.querySelector(".popup__button-close");
 
-function setPopupInput() {
-  nameInput.value = profileName;
-  JobInput.value = profileAbout;
-}
-
-function openPopup() {
-  editPopupElement.classList.add("popup_opened");
-}
-
-function handlePopupClick(event) {
-  openPopup();
-  setPopupInput();
-}
-
-function closePopup() {
-  editPopupElement.classList.remove("popup_opened");
-}
-
-function handleProfileFormSubmit(evt) {
-  evt.preventDefault();
-  profileNameElement.textContent = nameInput.value;
-  profileAboutElement.textContent = JobInput.value;
-  closePopup();
-}
-
-function likeButton() {}
-
-profileEditButton.addEventListener("click", handlePopupClick);
-closeButtonPopup.addEventListener("click", closePopup);
-formElement.addEventListener("submit", handleProfileFormSubmit);
+const editPopupImage = document.querySelector("#popup-add-card");
+const profileAddButton = document.querySelector(".profile__add-button");
+const newImageCloseButton = document.querySelector("#popup-close-add-button");
 
 const initialCards = [
   {
@@ -69,3 +44,47 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
   },
 ];
+
+function popupProfile() {
+  nameInput.value = profileName;
+  JobInput.value = profileAbout;
+  editPopupElement.classList.add("popup_opened");
+}
+function handlePopupClick(event) {
+  popupProfile();
+}
+function closeProfilePopup() {
+  editPopupElement.classList.remove("popup_opened");
+}
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileNameElement.textContent = nameInput.value;
+  profileAboutElement.textContent = JobInput.value;
+  closeProfilePopup();
+}
+
+//function handleOpenProfileForm() {
+//  editPopupElement.classList.add("popup_opened");
+//}
+//function handleCloseProfileForm() {
+//  editPopupElement.classList.remove("popup_opened");
+//}
+
+function handleOpenEditImage() {
+  editPopupImage.classList.add("popup_opened");
+}
+function closeEditImagePopup() {
+  editPopupImage.classList.remove("popup_opened");
+}
+
+function likeButton() {}
+
+profileEditButton.addEventListener("click", handlePopupClick);
+profileCloseButton.addEventListener("click", closeProfilePopup);
+formElement.addEventListener("submit", handleProfileFormSubmit);
+
+profileAddButton.addEventListener("click", handleOpenEditImage);
+newImageCloseButton.addEventListener("click", closeEditImagePopup);
+
+//profileEditButton.addEventListener("click", handleOpenProfileForm);
+//
