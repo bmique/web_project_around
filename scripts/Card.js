2bed247165529dbb9d4e59b9e92020a8a5de6da2
Betsabé Mique <betsabemique@gmail.com>
2024-03-21 21:05:56 -0300

    reogarnizando

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,26 +1,68 @@
 //export default
 
+const popupImage = document.querySelector("#popup-image");
+const popupImageView = document.querySelector(".popup__image-view");
+const imageCloseButton = document.querySelector("#popup-close-image");
+
 class Card {
-  constructor(title, url) {
-    this._title = title;
-    this._url = url;
+  constructor(name, link) {
+    this._name = name;
+    this._link = link;
   }
 
   _getCloneFromTemplate() {
     const cardElement = document
       .querySelector(".template-card")
-      .content.querySelector(".elements")
+      .content.querySelector(".element")
       .cloneNode(true);
 
     return cardElement;
   }
+
+  generateCard() {
+    this._element = this._getCloneFromTemplate();
+    this._setEventListeners();
+
+    this._element.querySelector("element__image").src = this._link;
+    this._element.querySelector(".element__image").alt = this._name;
+    this._element.querySelector(".element__name").textContent = this._name;
+
+    return this._element;
+  }
+
+  _handleOpenPopup() {
+    this._image = popupImageView.src;
+    popupImage.classList.add("popup_opened");
+  }
+
+  _handleClosePopup() {
+    popupImageView.src = " ";
+    popupImage.classList.remove("popup_opened");
+  }
+
+  _setEventListeners() {
+    this._element.addEventListener("click", () => {
+      this._handleOpenPopup();
+    });
+
+    imageCloseButton.addEventListener("click", () => {
+      this._handleClosePopup();
+    });
+  }
 }
+
+initialCards.forEach((card) => {
+  const card = new Card(card, ".template-card");
+  const cardElement = card.generateCard();
+
+  document.querySelector(".elements").append(cardElement);
+});
 
 // generateCard(obj) {
 //   this._element = this._getTemplate();
-//   this._element.querySelector(".element__image").src = this._url;
-//   this._element.querySelector(".element__image").alt = this._title;
-//   this._element.querySelector(".element__name").titleContent = this._title;
+//   this._element.querySelector(".element__image").src = this._link;
+//   this._element.querySelector(".element__image").alt = this._name;
+//   this._element.querySelector(".element__name").titleContent = this._name;
 //   this._setEventListeners(obj);
 //   return this._element;
 // }
@@ -33,47 +75,3 @@ class Card {
 //   document.addEventListener("keydown", obj.closeEscape);
 // }
 //}
-
-//////estos son los const que se deben llamar
-// const templatePokemon = document.querySelector(".template-pokemon");
-// const pokemonsArea = document.querySelector(".pokemons-area");
-
-// class Pokemon {
-//   constructor(number, nick) {
-//     this._number = number;
-//     this.nick = nick;
-//   }
-//   setPokemonProperties() {
-//     this._card = templatePokemon
-//       .cloneNode(true)
-//       .content.querySelector(".pokemon");
-//     this.btnDelete = this._card.querySelector(".pokemon__button_delete");
-//     this.btnAnimate = this._card.querySelector(".pokemon__button_animate");
-//     this.cardImage = this._card.querySelector(".pokemon__image");
-//     this.cardTitle = this._card.querySelector(".pokemon__title");
-//     this.cardTitle.textContent = this.nick;
-//     this.cardImage.src = "https............/${this._number}.png";
-//   }
-
-//   handleAnimation() {}
-//   handleDelete() {
-//     this._card.remove();
-//   }
-
-//   _setEventListeners() {
-//     this.btnDelete.addEventListener("click", () => {
-//       this.handleDelete();
-//     });
-//   }
-
-//   generatePokemon() {
-//     this.setPokemonProperties();
-//     this._setEventListeners();
-//     return this._card;
-//   }
-// }
-
-// const pokemonTest = new Pokemon(25, "pikachu");
-// const pokemonToRender = pokemonTest.generatePokemon;
-
-// pokemonsArea.append(pokemonToRender);
